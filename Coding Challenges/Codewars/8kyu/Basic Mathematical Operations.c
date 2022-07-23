2a6d3baac157895f1d32acffd1f73d4eb6fc13c4
Gabe <99471382+soaresgabe@users.noreply.github.com>
2022-07-22 23:05:40 -0300

    Create Basic Mathematical Operations.c

---
+++ Coding Challenges/Codewars/8kyu/Basic Mathematical Operations.c
@@ -0,0 +1,8 @@
+int basic_op(char op, int value1, int value2) {
+  switch (op) {
+      case '+': return value1 + value2;
+      case '-': return value1 - value2;
+      case '*': return value1 * value2;
+      case '/': return value1 / value2;
+  }
+}
