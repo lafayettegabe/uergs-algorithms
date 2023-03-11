d52e040576aeaf1327a561c82eca9759356b84e4
Gabriel Soares <gabriel-soares@uergs.edu.br>
2023-03-11 15:19:54 -0300

    2023/1 - aula01

--- Algorithms-II/001 - Array_inverter.cpp
+++ Algorithms-II/001 - Array_inverter.cpp
@@ -5,40 +5,33 @@
 
 #include <stdio.h>
 
-void mostrar(int arr);
-void inveter(int arr);
-
 int main() {
 	int arr[16], temp;
 	
 	// Recebe a array
-	for(int i = 0; i < 15; i++) {
+	for(int i = 0; i <= 15; i++) {
 		printf("Digita a posicao %d do vetor: ", i+1);
 		scanf("%d", &arr[i]);
 	}
 	
-	mostrar(arr);
-	
-	inveter(arr);
-	
-	mostrar(arr);
-}
-
-void mostrar(int arr) {	
 	// Mostra a array
-	for(int i = 0; i < 15, i++) {
-		printf("%d", arr[i])		
+	for(int i = 0; i <= 15; i++) {
+		printf("%d ", arr[i]);	
 	}
-	print("\n");
-}
-
-void inverter(int arr) {
+	printf("\n");
 	
+	// Inverte o array
 	for(int i = 0; i < 8; i++) {
 		temp      = arr[i];
-		arr[i]    = arr[16-i];
-		arr[16-i] = temp;
+		arr[i]    = arr[15-i];
+		arr[15-i] = temp;
 	}
 	
-	print("\n");
+	printf("\n");
+		
+	// Mostra a array	
+	for(int i = 0; i <= 15; i++) {
+		printf("%d ", arr[i]);		
+	}
+	printf("\n");
 }
